ecea47028b25554dcd87ee60cff5ed5bb5ca9063
Flammar <flammarr@gmail.com>
2019-10-15 01:28:47 +0300

    creation

---
+++ graph/src/test/java/name/skm/graph_test/graph/AppTest.java
@@ -0,0 +1,20 @@
+package name.skm.graph_test.graph;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
