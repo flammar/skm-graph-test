b159f1d306649d87019c71922bbe695441095367
Skidan, Mikhail <Mikhail.Skidan@emc.com>
2019-10-16 00:53:34 +0300

    Some code cleaning

--- graph/src/main/java/name/skm/graph_test/graph/Graph.java
+++ graph/src/main/java/name/skm/graph_test/graph/Graph.java
@@ -10,10 +10,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import lombok.Data;
 import lombok.NonNull;
