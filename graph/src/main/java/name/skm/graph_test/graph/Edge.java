430b82c08303c9f847a9dbffde32a1ece10d863a
Skidan, Mikhail <Mikhail.Skidan@emc.com>
2019-10-21 12:37:20 +0300

    Lombok dependency removed

--- graph/src/main/java/name/skm/graph_test/graph/Edge.java
+++ graph/src/main/java/name/skm/graph_test/graph/Edge.java
@@ -1,21 +1,54 @@
 package name.skm.graph_test.graph;
 
-import lombok.Data;
-import lombok.NonNull;
-
-@Data
 public class Edge<T> {
     public Edge(T from, T to) {
         this.from = from;
         this.to = to;
     }
 
-    @NonNull
     private T from;
-    @NonNull
     private T to;
 
     public Edge<T> getReversed() {
         return new Edge<T>(to, from);
+    }
+
+    public T getFrom() {
+        return from;
+    }
+
+    public T getTo() {
+        return to;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((from == null) ? 0 : from.hashCode());
+        result = prime * result + ((to == null) ? 0 : to.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Edge<?> other = (Edge<?>) obj;
+        if (from == null) {
+            if (other.from != null)
+                return false;
+        } else if (!from.equals(other.from))
+            return false;
+        if (to == null) {
+            if (other.to != null)
+                return false;
+        } else if (!to.equals(other.to))
+            return false;
+        return true;
     }
 }
